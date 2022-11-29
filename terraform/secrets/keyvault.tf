a589a73d920537b4449370b29845d923f4e5a6e1
James Byrne <jambyr@bas.ac.uk>
2022-11-29 15:45:15 +0000

    Fixing #29: sourcing object ID for newer CLI from azuread data provider

--- terraform/secrets/keyvault.tf
+++ terraform/secrets/keyvault.tf
@@ -25,11 +25,11 @@ resource "azurerm_key_vault_access_policy" "allow_group" {
 }
 
 # Set the KeyVault permissions for the current user
-data "azurerm_client_config" "current" {}
+data "azuread_client_config" "current" {}
 resource "azurerm_key_vault_access_policy" "allow_user" {
   key_vault_id       = azurerm_key_vault.this.id
   tenant_id          = var.tenant_id
-  object_id          = data.azurerm_client_config.current.object_id
+  object_id          = data.azuread_client_config.current.object_id
   key_permissions    = var.key_permissions
   secret_permissions = var.secret_permissions
 }
