b2c56e52c72e4ec4726138241916b705dc8dd79f
James Byrne <jambyr@bas.ac.uk>
2023-04-28 14:12:23 +0100

    Dev #33: refactoring to v3

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,70 +7,70 @@ module "network" {
 }
 
 # Secrets module
-module "secrets" {
-  source              = "./secrets"
-  developers_group_id = var.developers_group_id
-  location            = var.location
-  project_name        = local.project_name
-  default_tags        = local.tags
-  tenant_id           = var.tenant_id
-}
+#module "secrets" {
+#  source              = "./secrets"
+#  developers_group_id = var.developers_group_id
+#  location            = var.location
+#  project_name        = local.project_name
+#  default_tags        = local.tags
+#  tenant_id           = var.tenant_id
+#}
 
 # Data storage
-module "data" {
-  source = "./data"
-  default_tags        = local.tags
-  location            = var.location
-  project_name        = local.project_name
-  subnet              = module.network.public_subnet
-}
+#module "data" {
+#  source = "./data"
+#  default_tags        = local.tags
+#  location            = var.location
+#  project_name        = local.project_name
+#  subnet              = module.network.public_subnet
+#}
 
 # Logging module
-module "logging" {
-  source = "./logging"
-  default_tags        = local.tags
-  location            = var.location
-  project_name        = local.project_name
-}
+#module "logging" {
+#  source = "./logging"
+#  default_tags        = local.tags
+#  location            = var.location
+#  project_name        = local.project_name
+#}
 
 # Database module
-module "database" {
-  source               = "./database"
-  resource_group_name  = module.data.rg_name
-  location             = var.location
-  project_name         = local.project_name
-  storage_mb           = 8192
-  key_vault_id         = module.secrets.key_vault_id
-  logging_workspace_id = module.logging.logging_workspace_id
-  database_names       = local.database_names
-  default_tags         = local.tags
-  subnet               = module.network.private_subnet
-}
+#module "database" {
+#  source               = "./database"
+#  resource_group_name  = module.data.rg_name
+#  location             = var.location
+#  project_name         = local.project_name
+#  storage_mb           = 8192
+#  key_vault_id         = module.secrets.key_vault_id
+#  logging_workspace_id = module.logging.logging_workspace_id
+#  database_names       = local.database_names
+#  default_tags         = local.tags
+#  subnet               = module.network.private_subnet
+#}
 
 # NetCDF processing
-module "processing" {
-  source                       = "./processing"
-  data_storage_account         = module.data.storage_account
-  database_resource_group_name = module.data.rg_name
-  database_fqdn                = module.database.server_fqdn
-  database_host                = module.database.server_name
-  database_name                = local.database_names[0]
-  database_user                = module.database.admin_username
-  database_password            = module.database.admin_password
-  location                     = var.location
-  project_name                 = local.project_name
-  default_tags                 = local.tags
-  subnet                       = module.network.private_subnet
-}
+#module "processing" {
+#  source                       = "./processing"
+#  data_storage_account         = module.data.storage_account
+#  database_resource_group_name = module.data.rg_name
+#  database_fqdn                = module.database.server_fqdn
+#  database_host                = module.database.server_name
+#  database_name                = local.database_names[0]
+#  database_user                = module.database.admin_username
+#  database_password            = module.database.admin_password
+#  location                     = var.location
+#  project_name                 = local.project_name
+#  default_tags                 = local.tags
+#  subnet                       = module.network.private_subnet
+#}
 
 # Event grid topics for integrations
-module "events" {
-  source                       = "./events"
-
-  resource_group_name          = module.processing.rg_name
-  storage_resource_group_name  = module.data.rg_name
-  location                     = var.location
-  project_name                 = local.project_name
-  default_tags                 = local.tags
-  storage_id                   = module.data.storage_account.id
-}
+#module "events" {
+#  source                       = "./events"
+#
+#  resource_group_name          = module.processing.rg_name
+#  storage_resource_group_name  = module.data.rg_name
+#  location                     = var.location
+#  project_name                 = local.project_name
+#  default_tags                 = local.tags
+#  storage_id                   = module.data.storage_account.id
+#}
